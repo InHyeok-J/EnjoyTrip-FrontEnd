f01b0ef7c6ae886dff328666d876d788daac6350
synodical <lena0323park@naver.com>
2023-05-23 17:19:20 +0900

    getDetail, getReviews api

--- src/api/attractionApi.js
+++ src/api/attractionApi.js
@@ -4,6 +4,14 @@ export const getGuguns = async (sidoCode) => {
   return (await http.get("/attractions/sidos/" + sidoCode)).data;
 };
 
+export const getDetail = async (attractionId) => {
+  return (await http.get("/attractions/" + attractionId)).data;
+};
+
+export const getReviews = async (attractionId) => {
+  return (await http.get("/attractions/" + attractionId + "/reviews")).data;
+};
+
 export const search = async (params) => {
   let url = "/attractions/?";
   if (params.sidoCode) {
