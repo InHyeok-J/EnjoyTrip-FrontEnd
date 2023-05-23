a1a71512738a37158b5eec991c2d44f8397d9374
synodical <lena0323park@naver.com>
2023-05-23 23:09:03 +0900

    Fix : login 화면 탭 바 삭제

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,16 @@
 <template>
-	<div>
-		<not-logo-header></not-logo-header>
-		<login-home></login-home>
-		<common-tab></common-tab>
-	</div>
+  <div>
+    <not-logo-header></not-logo-header>
+    <login-home></login-home>
+  </div>
 </template>
 
 <script>
-import LoginHome from '@/components/login/LoginHome.vue';
-import NotLogoHeader from '@/components/common/NotLogoHeader.vue';
-import CommonTab from '@/components/common/CommonTab.vue';
+import LoginHome from "@/components/login/LoginHome.vue";
+import NotLogoHeader from "@/components/common/NotLogoHeader.vue";
 export default {
-	name: 'LoginView',
-	components: { LoginHome, NotLogoHeader, CommonTab },
+  name: "LoginView",
+  components: { LoginHome, NotLogoHeader },
 };
 </script>
 
